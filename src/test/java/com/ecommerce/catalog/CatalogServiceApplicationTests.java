59916991faa0037f828ef276ffc4383fc43210a7
cos4h <olvadis2004@gmail.com>
2024-09-23 08:51:09 -0500

    ci/cd workflow

--- src/test/java/com/ecommerce/catalog/CatalogServiceApplicationTests.java
+++ src/test/java/com/ecommerce/catalog/CatalogServiceApplicationTests.java
@@ -2,12 +2,12 @@ package com.ecommerce.catalog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CatalogServiceApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
